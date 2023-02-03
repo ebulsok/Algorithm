8de1f13f4175f678bac44fb2acfbc41bcaf086bc
ebulsok <96829798+ebulsok@users.noreply.github.com>
2023-02-03 10:55:23 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,12 @@
+function solution(num_list, n) {
+    let answer = [];
+    let temp = [];
+    for(let i = 0; i < num_list.length; i++) {
+        temp.push(num_list[i]);
+        if((i + 1) % n === 0) {
+            answer.push(temp);
+            temp = [];
+        }
+    }
+    return answer
+}
