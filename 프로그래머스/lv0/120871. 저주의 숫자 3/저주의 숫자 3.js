b042a7e8c85859e20b2f6171bde1cb7906aa3623
Hyeyoung Park <96829798+ebulsok@users.noreply.github.com>
2023-02-15 09:52:13 +0900

    [level 0] Title: 저주의 숫자 3, Time: 3.48 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = 1;
+    for(let i = 2; i <= n; i++) {
+        answer++;
+        if(answer % 3 === 0 || (answer.toString().includes('3'))) answer = three(answer);
+        console.log(i, answer)
+    }
+    return answer;
+}
+
+function three(n) {
+    if(n % 3 === 0 || (n.toString().includes('3'))) return three(n+1);
+    return n
+}
