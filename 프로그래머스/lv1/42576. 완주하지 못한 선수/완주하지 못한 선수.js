34ac9a0d0f899cb82baab8d14d9988ac3a64573d
Hyeyoung Park <96829798+ebulsok@users.noreply.github.com>
2023-03-08 19:27:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 101.82 ms, Memory: 57.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,10 @@
+function solution(participant, completion) {
+    let answer = '';
+    participant.sort();
+    completion.sort();
+    for(let i = 0; i < completion.length; i++) {
+        if(completion[i] !== participant[i]) return participant[i];
+    }
+    return participant[participant.length - 1];
+
+}
